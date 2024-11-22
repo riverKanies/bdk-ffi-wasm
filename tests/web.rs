8d80a92dfca58e977f98b9e69c2e6c3dd0eee97d
River Kanies <river.kanies@gmail.com>
2024-11-22 15:59:16 -0600

    use Descriptor for wallet

--- tests/web.rs
+++ tests/web.rs
@@ -4,36 +4,26 @@
 
 extern crate wasm_bindgen_test;
 
-use anyhow::{anyhow, Error};
-use std::str::FromStr;
 use web_sys::console;
 
-use bdk_wallet::{
-    bip39::Mnemonic,
-    bitcoin::{bip32::DerivationPath, key::Secp256k1, AddressType, Network},
-    descriptor,
-    descriptor::IntoWalletDescriptor,
-};
 use bdk_ffi_wasm::{Descriptor, WalletWrapper};
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 fn new_test_wallet() -> Result<WalletWrapper, String> {
-    let mnemonic_str = "drip drum plug universe beyond gasp cram action hurt keep awake tortoise luggage return luxury net jar awake mimic hurry critic curtain quiz kit";
     let esplora_url = "https://mutinynet.com/api";
 
-    let mnemonic = Mnemonic::from_str(mnemonic_str).unwrap();
-    let (descriptor, change_descriptor) =
-        mnemonic_to_descriptor(mnemonic, Network::Signet, AddressType::P2wpkh).expect("descriptor");
+    let descriptor = Descriptor::new("tr([12071a7c/86'/1'/0']tpubDCaLkqfh67Qr7ZuRrUNrCYQ54sMjHfsJ4yQSGb3aBr1yqt3yXpamRBUwnGSnyNnxQYu7rqeBiPfw3mjBcFNX4ky2vhjj9bDrGstkfUbLB9T/0/*)#z3x5097m".into(), "signet".into()).expect("descriptor");
+    console::log_1(&format!("descriptor parsed: {}", descriptor.to_string_with_secret()).into());
 
-    console::log_1(&format!("descriptor: {}", descriptor).into());
-    console::log_1(&format!("change_descriptor: {}", change_descriptor).into());
+    let change_descriptor = Descriptor::new("tr([12071a7c/86'/1'/0']tpubDCaLkqfh67Qr7ZuRrUNrCYQ54sMjHfsJ4yQSGb3aBr1yqt3yXpamRBUwnGSnyNnxQYu7rqeBiPfw3mjBcFNX4ky2vhjj9bDrGstkfUbLB9T/1/*)#n9r4jswr".into(), "signet".into()).expect("descriptor");
+    console::log_1(&format!("change_descriptor parsed: {}", change_descriptor.to_string_with_secret()).into());
 
     WalletWrapper::new(
         "signet".into(),
-        descriptor,
-        change_descriptor,
+        descriptor.to_string_with_secret(),
+        change_descriptor.to_string_with_secret(),
         esplora_url.to_string(),
     )
 }
@@ -41,8 +31,6 @@ fn new_test_wallet() -> Result<WalletWrapper, String> {
 #[wasm_bindgen_test]
 async fn test_wallet() {
 
-    let descriptor = Descriptor::new("tr([12071a7c/86'/1'/0']tpubDCaLkqfh67Qr7ZuRrUNrCYQ54sMjHfsJ4yQSGb3aBr1yqt3yXpamRBUwnGSnyNnxQYu7rqeBiPfw3mjBcFNX4ky2vhjj9bDrGstkfUbLB9T/0/*)#z3x5097m".into(), "signet".into()).expect("descriptor");
-    console::log_1(&format!("descriptor parsed: {}", descriptor.to_string_with_secret()).into());
 
 
     let wallet = new_test_wallet().expect("wallet");
@@ -51,75 +39,12 @@ async fn test_wallet() {
     let first_address = wallet.peek_address(0);
     assert_eq!(
         first_address,
-        "tb1q8vl3qjdxnm54psxn5vgzdf402ky23r0jjfd8cj".to_string()
+        "tb1pkar3gerekw8f9gef9vn9xz0qypytgacp9wa5saelpksdgct33qdqs257jl".to_string()
     );
 
     let balance = wallet.balance();
-    assert_eq!(balance, 0);
+    assert_ne!(balance, 0);
 
     let new_address = wallet.get_new_address();
     console::log_1(&format!("new_address: {}", new_address).into());
-}
-
-pub fn mnemonic_to_descriptor(
-    mnemonic: Mnemonic,
-    network: Network,
-    address_type: AddressType,
-) -> Result<(String, String), Error> {
-    let mnemonic_with_passphrase = (mnemonic, None);
-    let secp = Secp256k1::new();
-
-    let purpose = match address_type {
-        AddressType::P2wpkh => "84h",
-        AddressType::P2tr => "86h",
-        AddressType::P2sh => "49h",
-        _ => "44h",
-    };
-
-    let coin_type: &str = match network {
-        Network::Bitcoin => "0h",
-        _ => "1h",
-    };
-
-    let external_path = DerivationPath::from_str(&format!("m/{}/{}/0h/0", purpose, coin_type))?;
-    let internal_path = DerivationPath::from_str(&format!("m/{}/{}/0h/1", purpose, coin_type))?;
-
-    let (external_descriptor, internal_descriptor) = match address_type {
-        AddressType::P2pkh => {
-            let ext = descriptor!(pk((mnemonic_with_passphrase.clone(), external_path)))?;
-            let int = descriptor!(pk((mnemonic_with_passphrase, internal_path)))?;
-            (ext, int)
-        }
-        AddressType::P2sh => {
-            let ext = descriptor!(sh(wpkh((mnemonic_with_passphrase.clone(), external_path))))?;
-            let int = descriptor!(sh(wpkh((mnemonic_with_passphrase, internal_path))))?;
-            (ext, int)
-        }
-        AddressType::P2wpkh => {
-            let ext = descriptor!(wpkh((mnemonic_with_passphrase.clone(), external_path)))?;
-            let int = descriptor!(wpkh((mnemonic_with_passphrase, internal_path)))?;
-            (ext, int)
-        }
-        AddressType::P2tr => {
-            let ext = descriptor!(tr((mnemonic_with_passphrase.clone(), external_path)))?;
-            let int = descriptor!(tr((mnemonic_with_passphrase, internal_path)))?;
-            (ext, int)
-        }
-        _ => {
-            return Err(anyhow!("Unsupported address type"));
-        }
-    };
-
-    let (external_wallet_descriptor, _) =
-        external_descriptor.into_wallet_descriptor(&secp, network)?;
-    let (internal_wallet_descriptor, _) =
-        internal_descriptor.into_wallet_descriptor(&secp, network)?;
-
-    external_wallet_descriptor.sanity_check()?;
-    internal_wallet_descriptor.sanity_check()?;
-
-    Ok((
-        external_wallet_descriptor.to_string(),
-        internal_wallet_descriptor.to_string(),
-    ))
 }
